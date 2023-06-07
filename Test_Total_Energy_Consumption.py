1654d5c839db86784c1f6fc298eaae000ea265c9
Amal Ibrahim <amlselimv@gmail.com>
2023-06-07 19:55:00 +0200

    change test code

--- Test_Total_Energy_Consumption.py
+++ Test_Total_Energy_Consumption.py
@@ -1,20 +1,16 @@
 import unittest
-import matplotlib.pyplot as plt
-
-# Import the class to be tested
 from energy_consumption_chart import EnergyConsumptionChart
 
 class TestEnergyConsumptionChart(unittest.TestCase):
-    def test_plot_chart(self):
+    def test_total_values(self):
         renewable_data = [50, 60, 70, 80, 90]
         non_renewable_data = [150, 140, 130, 120, 110]
         years = [2015, 2016, 2017, 2018, 2019]
 
         chart = EnergyConsumptionChart(renewable_data, non_renewable_data, years)
-        chart.plot_chart()
 
-        # Assert that the chart is displayed
-        self.assertTrue(plt.gcf().number > 0)
+        total_renewable = sum(renewable_data)
+        total_non_renewable = sum(non_renewable_data)
 
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(chart.get_total_renewable(), total_renewable)
+        self.assertEqual(chart.get_total_non_renewable(), total_non_renewable)
